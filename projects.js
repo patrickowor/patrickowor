b3862d8d788dd4b16455493b34832d2f531fee65
Owor Patrick Ikongha <patrick.ikongha@gmail.com>
2024-04-14 23:43:14 +0100

    added project details

--- projects.js
+++ projects.js
@@ -3,6 +3,7 @@ var projects = [
         name : "manilas market",
         type : "web",
         image : "assets/img/portfolio/portfolio-1.png",
+        moreimg : ["assets/img/portfolio/mandilas-2.png", "assets/img/portfolio/mandilas-3.png", "assets/img/portfolio/mandilas-api.png"],
         link : "https://mandilasmarket.com",
         client : "StrongWave Intl",
         about: `
@@ -15,6 +16,7 @@ var projects = [
         type : "web",
         other : "ai",
         image : "assets/img/portfolio/braincize-1.png",
+        moreimg : [],
         link : "https://braincize.com",
         client : "Dr. Calvin H Flowers M.D",
         about : `
@@ -26,6 +28,7 @@ var projects = [
         type : "app",
         image : "assets/img/portfolio/coockeroo-1.png",
         link : "https://play.google.com/store/apps/details?id=com.erp.coockeroo&hl=en_IE",
+        moreimg : ["assets/img/portfolio/coockeroo-3.png"],
         client : "Constantin Schmidt",
         about : `The Coockeroo Cashier & ERP Application is what Mom'n'Pop stores have been waiting for.
         It takes the whole management process that produces tons of paper & time waste and puts it into the palm of your hand.
@@ -42,18 +45,28 @@ var projects = [
         name : "IMMERSIVE",
         type : "web",
         image : "assets/img/portfolio/universe-1.png",
+        moreimg : [ "assets/img/portfolio/universe-2.png", "assets/img/portfolio/universe-3.png"],
         link : "https://universe-taupe.vercel.app",
-        client : "",
-        about : ``
+        client : "Refine Studio",
+        about : `
+            A proof of concept designed for refine studio 
+            front end developers interview. 
+        `
     },
     {
         name : "MEDI PLANT",
         type : "app",
         other : "ai",
-        image : "assets/img/portfolio/1.PNG",
+        image : "assets/img/portfolio/1 copy.PNG",
+        moreimg : ["assets/img/portfolio/2.PNG"],
         link : "https://github.com/patrickowor/med_plant_backend",
         client : "personal",
-        about : `
+        about : `Mediplant is your gateway to the rich heritage of African medicinal plants. We leverage cutting-edge AI and computer vision technology to empower you to identify these potent plants with ease.<br />
+
+        Our user-friendly platform takes the guesswork out of plant identification.<br /> Simply upload a picture or point your camera at the plant, and Mediplant will instantly reveal its traditional uses and potential health benefits.<br />
+        
+        Mediplant is more than just an identification tool. We bridge the gap between ancient knowledge and modern science, providing you with reliable information on these natural wonders.<br />
+        Join us on a journey to explore the healing power of African plants. Let Mediplant be your guide to a world of botanical wisdom.
         `
     },
     {
@@ -61,9 +74,17 @@ var projects = [
         type : "web",
         other : "ai",
         image : "assets/img/portfolio/ava-1.png",
+        moreimg : ["assets/img/portfolio/ava-2.png"],
         link : "https://ava-nine.vercel.app/",
         client : "SourceBird developers",
         about : `
+        AVA isn't your average chat companion. We harness the potential of cutting-edge language models to deliver an unparalleled chat and text generation experience.<br />
+
+Imagine having witty conversations, crafting captivating stories, or even generating creative content - all with AVA by your side. Simply spark a conversation, provide a prompt, and AVA will weave its magic, crafting engaging text tailored to your needs.<br />
+
+Whether you're seeking a stimulating dialogue partner, a spark for your next writing project, or simply a fun way to explore the possibilities of language, AVA is here.<br />
+
+Go beyond basic chat. Experience the future of language interaction with AVA.<br />
         `
     },
     {
@@ -71,9 +92,16 @@ var projects = [
         type : "web",
         other : "3d",
         image : "assets/img/portfolio/pool3d-1.png",
+        moreimg : ["assets/img/portfolio/pool3d-2.png", "assets/img/portfolio/pool3d-3.png"],
         link : "https://pool3d.vercel.app/",
         client : "Freelance",
-        about : `#3898ec`
+        about : `Sharpen your cue and step into the virtual pool hall with Pool3D! This immersive proof-of-concept website utilizes cutting-edge Three.js graphics to deliver a stunning and realistic 3D pool experience.<br />
+
+        Forget clunky downloads - Pool3D lets you jump right into the action directly from your browser. Challenge your friends to a quick game of 8-ball or hone your skills in solo practice mode.<br />
+        
+        Pool3D boasts intuitive controls and lifelike physics, making every shot feel satisfyingly real. So, whether you're a seasoned pool shark or a curious newcomer, Pool3D offers an engaging and accessible way to test your skills.<br />
+        
+        Dive in, rack 'em up, and experience the thrill of the game in a whole new dimension with Pool3D!<br />`
     },
 ]
 
